7d532ae09de7d189be177ab3b567db9eafe032ad
krm2338 <krm2338@gmail.com>
2012-06-28 09:27:28 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Kirk::Application.config.secret_token = '16e1a250f9e26d09425ba9ad93e09226c35143d3561891b5fdff147c989c0b0ef7a3934a49c77382846b061941a3c94a9570ddd827961e757c61e74c2689669b'
